119fe136ec3f8b874bc7815d67a7e2034f9750a0
Benjamin Machlev <beni251@gmail.com>
2024-11-03 14:37:00 +0200

    add new fields

--- movie.py
+++ movie.py
@@ -1,12 +1,17 @@
 from rating import Rating
 class Movie:
-    def __init__(self,movieID, title, director, release_year, genre, rating):
+    def __init__(self,movieID, title, director, release_year, genre, rating,
+                 runtime, description, response, image):
         self.movieID = movieID
         self.title = title
         self.director = director
         self.release_year = release_year
         self.genre = genre
         self.rating = rating
+        self.runtime = runtime 
+        self.description = description
+        self.response = response
+        self.image = image
 
     def __str__(self):
         return f"{self.title} ({self.release_year}), directed by {self.director}, Genre: {self.genre}, Rating: {self.rating}/10"
