b639f78e58c788cd0109cece7f25bb92a0ff8e9b
Benjamin Machlev <beni251@gmail.com>
2024-11-06 17:29:29 +0200

    resize img

--- MovieView/movieView.py
+++ MovieView/movieView.py
@@ -183,17 +183,21 @@ class MovieView(QMainWindow):
 
     def create_movie_frame(self, movie):
         frame = QFrame()
-        frame.setFixedSize(100, 150)
+        frame.setFixedSize(220, 325)
         layout = QVBoxLayout(frame) 
         
         image_button = QPushButton()
+        image_button.setStyleSheet("border: 5px solid;")
         image_button.setIcon(QPixmap(movie.image)) 
-        image_button.setIconSize(QSize(100, 80))  
-        image_button.setFixedSize(QSize(100, 80))
+        image_button.setIconSize(QSize(210, 315))  
+        image_button.setFixedSize(QSize(210, 315))
         image_button.clicked.connect(lambda _, m=movie: self.show_movie(m))  
 
-        title_label = QLabel(f"{movie.title} <br> ({movie.release_year})")
-        title_label.setAlignment(Qt.AlignCenter)  
+        title_label = QLabel(f"<b>{movie.title}</b><br>({movie.release_year})")
+          
+
+        layout.addWidget(image_button, alignment=Qt.AlignCenter)
+
         
         layout.addWidget(image_button, alignment=Qt.AlignCenter)
         layout.addWidget(title_label, alignment=Qt.AlignCenter)
