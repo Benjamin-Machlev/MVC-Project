27c7b736086211db51642b8727f8d6395d24a0b3
Benjamin Machlev <beni251@gmail.com>
2024-11-13 17:06:52 +0200

    move frontend files to new folder

--- Front-End/main.py
+++ Front-End/main.py
@@ -7,7 +7,7 @@ from MovieController.movieController import MovieController
 
 def main():
     app = QApplication([])
-    with open(r"MovieView\style.qss", "r",encoding="utf-8") as style_file:
+    with open(r"Front-End\MovieView\style.qss", "r",encoding="utf-8") as style_file:
         app.setStyleSheet(style_file.read()) 
 
     model = MovieModel()
