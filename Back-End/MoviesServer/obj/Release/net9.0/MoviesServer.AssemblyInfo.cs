3ee8b2f61672158feb974aa55eaa4494e1470409
Benjamin Machlev <beni251@gmail.com>
2024-11-14 12:09:49 +0200

    start a server side

--- Back-End/MoviesServer/obj/Release/net9.0/MoviesServer.AssemblyInfo.cs
+++ Back-End/MoviesServer/obj/Release/net9.0/MoviesServer.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoviesServer")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44ce422a112cd4c344aec5f353716a005580d8d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoviesServer")]
