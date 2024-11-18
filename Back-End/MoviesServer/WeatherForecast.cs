9dd0afaea61181fcf2efe704cc3ab478d7c8604a
Benjamin Machlev <beni251@gmail.com>
2024-11-18 13:27:56 +0200

    add movie filds

--- Back-End/MoviesServer/WeatherForecast.cs
+++ Back-End/MoviesServer/WeatherForecast.cs
@@ -1,13 +1,33 @@
+using static System.Net.Mime.MediaTypeNames;
+using System.IO;
+
 namespace MoviesServer
 {
-    public class WeatherForecast
+    public class Movie
     {
-        public DateOnly Date { get; set; }
+        public int MovieID { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+        public int Runtime { get; set; }
+        public string Description { get; set; }
+        public string Responses { get; set; }
+        public string Image { get; set; }
 
-        public int TemperatureC { get; set; }
-
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string? Summary { get; set; }
+        public Movie(int movieID, string title, string director, int releaseYear, string genre, double rating, int runtime, string description, string responses, string image)
+        {
+            MovieID = movieID;
+            Title = title;
+            Director = director;
+            ReleaseYear = releaseYear;
+            Genre = genre;
+            Rating = rating;
+            Runtime = runtime;
+            Description = description;
+            Responses = responses;
+            Image = image;
+        }
     }
 }
