9543ff1108b855734442fce355f401b6dd60c862
Benjamin Machlev <beni251@gmail.com>
2024-11-27 17:12:02 +0200

    Add theme colors in my_theme.xml and update main.py to apply new stylesheet

--- Front-End/main.py
+++ Front-End/main.py
@@ -4,13 +4,16 @@ from MovieModel.movieModel import MovieModel
 from MovieView.movieView import MovieView
 from MovieController.movieController import MovieController
 from qt_material import apply_stylesheet
+from qt_material import list_themes
 
 def main():
     app = QApplication([])
     with open(r"Front-End\MovieView\style.qss", "r", encoding="utf-8") as style_file:
         app.setStyleSheet(style_file.read())
 
-    #apply_stylesheet(app, theme='dark_teal.xml')
+    list_themes()
+    #print(list_themes())
+    apply_stylesheet(app, theme='dark_medical.xml')
     model = MovieModel()
     view = MovieView(None, model.movies)  # Initialize view first
     controller = MovieController(model, view)  # Pass view to controller
