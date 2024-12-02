55891e196e3e3c0df3b36ccc7ecd0d7a3961b338
Benjamin Machlev <beni251@gmail.com>
2024-12-02 15:28:54 +0200

    Add dependency injection and AddMovie method to CreateMovieCommand
    Introduced the MoviesServer.DataAccess namespace and added a private
    readonly field `_context` of type `MoviesContext`. Added a constructor
    to the `CreateMovieCommand` class to initialize `_context`. Implemented
    the `AddMovie` method to add a new movie to the database and save the
    changes.

--- Back-End/MoviesServer/CQRS/Commands/CreateMovieCommand.cs
+++ Back-End/MoviesServer/CQRS/Commands/CreateMovieCommand.cs
@@ -1,6 +1,22 @@
-﻿namespace MoviesServer.CQRS.Commands
+﻿
+using MoviesServer.DataAccess;
+using MoviesServer.Models;
+
+namespace MoviesServer.CQRS.Commands
 {
     public class CreateMovieCommand
     {
+        private readonly MoviesContext _context;
+
+        public CreateMovieCommand(MoviesContext context)
+        {
+            _context = context;
+
+        }
+        public void AddMovie(Movie movie)
+        {
+             _context.Movies.Add(movie);
+            _context.SaveChanges();
+        }
     }
 }
