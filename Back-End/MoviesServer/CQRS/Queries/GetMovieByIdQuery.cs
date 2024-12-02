ca8df209a71bb1850d726cf59617c2dc6a8282e7
Benjamin Machlev <beni251@gmail.com>
2024-12-02 15:29:56 +0200

    Implement GetMovieByIdQuery with database access
    Introduced a new implementation for the GetMovieByIdQuery class in the MoviesServer.CQRS.Queries namespace. Imported MoviesContext and Movie classes from MoviesServer.DataAccess and MoviesServer.Models respectively. Added a private readonly field _context of type MoviesContext and initialized it via a constructor. Added the GetMovieById method to fetch a Movie by its ID from the database, returning null if not found.

--- Back-End/MoviesServer/CQRS/Queries/GetMovieByIdQuery.cs
+++ Back-End/MoviesServer/CQRS/Queries/GetMovieByIdQuery.cs
@@ -1,6 +1,25 @@
-﻿namespace MoviesServer.CQRS.Queries
+﻿using MoviesServer.DataAccess;
+using MoviesServer.Models;
+namespace MoviesServer.CQRS.Queries
+
 {
     public class GetMovieByIdQuery
     {
+        readonly MoviesContext _context;
+
+        public GetMovieByIdQuery(MoviesContext context)
+        {
+            _context = context;
+        }
+
+        public Movie? GetMovieById(int id)
+        {
+            var movie = _context.Movies.Find(id);
+            if (movie == null)
+            {
+                return null;
+            }
+            return movie;
+        }
     }
 }
