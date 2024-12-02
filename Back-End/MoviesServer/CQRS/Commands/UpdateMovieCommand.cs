3d962ab61d9c631b8c40cdf4188321234cebf9cd
Benjamin Machlev <beni251@gmail.com>
2024-12-02 15:29:30 +0200

    Add UpdateMovieCommand with UpdateMovie method
    - Modified namespace in UpdateMovieCommand.cs to include additional using directives for MoviesServer.DataAccess and MoviesServer.Models.
    - Added a private readonly field _context of type MoviesContext.
    - Implemented a constructor to initialize the _context field.
    - Added UpdateMovie method to check id and update Movie object, then save changes to the database.

--- Back-End/MoviesServer/CQRS/Commands/UpdateMovieCommand.cs
+++ Back-End/MoviesServer/CQRS/Commands/UpdateMovieCommand.cs
@@ -1,6 +1,24 @@
-﻿namespace MoviesServer.CQRS.Commands
+﻿using MoviesServer.DataAccess;
+using MoviesServer.Models;
+namespace MoviesServer.CQRS.Commands
 {
     public class UpdateMovieCommand
     {
+        private readonly MoviesContext _context;
+
+        public UpdateMovieCommand(MoviesContext context)
+        {
+            _context = context;
+        }
+
+        public void UpdateMovie(int id, Movie movie)
+        {
+            if (id != movie.MovieID)
+            {
+                throw new System.Exception("Invalid movie ID");
+            }
+            _context.Entry(movie).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+        }
     }
 }
