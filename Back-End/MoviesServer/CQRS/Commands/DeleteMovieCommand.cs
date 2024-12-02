d1ac6cbba2c74eccd79d711d95dd756ccc8a37a2
Benjamin Machlev <beni251@gmail.com>
2024-12-02 15:29:16 +0200

    Add DeleteMovieCommand with dependency injection and method
    Updated DeleteMovieCommand to include necessary using directives
    and a constructor for dependency injection of MoviesContext.
    Added DeleteMovie method to handle movie deletion by ID with
    error handling for invalid IDs.

--- Back-End/MoviesServer/CQRS/Commands/DeleteMovieCommand.cs
+++ Back-End/MoviesServer/CQRS/Commands/DeleteMovieCommand.cs
@@ -1,6 +1,26 @@
-﻿namespace MoviesServer.CQRS.Commands
+﻿using MoviesServer.DataAccess;
+using MoviesServer.Models;
+
+namespace MoviesServer.CQRS.Commands
 {
     public class DeleteMovieCommand
     {
+        private readonly MoviesContext _context;
+
+        public DeleteMovieCommand(MoviesContext context)
+        {
+            _context = context;
+        }
+
+        public void DeleteMovie(int id)
+        {
+            var movie = _context.Movies.Find(id);
+            if (movie == null)
+            {
+                throw new System.Exception("Invalid movie ID");
+            }
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
+        }
     }
 }
