fb5894655d2d70c80d6d2e2c0db31b2fb37f1947
Benjamin Machlev <beni251@gmail.com>
2024-12-02 15:29:45 +0200

    Add GetAllMoviesQuery with dependency injection
    Introduced `MoviesServer.DataAccess` and `MoviesServer.Models` namespaces.
    Added `_context` field to `GetAllMoviesQuery` for `MoviesContext`.
    Created constructor to initialize `_context`.
    Implemented `GetAllMovies` method to fetch all movies from the database.

--- Back-End/MoviesServer/CQRS/Queries/GetAllMoviesQuery.cs
+++ Back-End/MoviesServer/CQRS/Queries/GetAllMoviesQuery.cs
@@ -1,6 +1,20 @@
-﻿namespace MoviesServer.CQRS.Queries
+﻿using MoviesServer.DataAccess;
+using MoviesServer.Models;
+
+
+namespace MoviesServer.CQRS.Queries
 {
     public class GetAllMoviesQuery
     {
+        readonly MoviesContext _context;
+        public GetAllMoviesQuery(MoviesContext context)
+        {
+            _context = context;
+        }
+
+        public List<Movie> GetAllMovies()
+        {
+            return _context.Movies.ToList();
+        }
     }
 }
